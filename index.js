43ca941518fb3b87f980b8b09f5cbf00b0e8356c
Mr-Bie <Mrb800424@gmail.com>
2022-11-27 14:49:18 +0330

    feat: necessary directories & files creation added to app starting point (index.js)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//Dependencies
+require("dotenv").config();
+const path = require("path");
+
+//Utils
+const {
+  directoryCreator: createDirectory,
+  fileCreatorSync: createFile,
+  fileCopierSync: copyFile,
+} = require("./utils/directory.util");
+
+//Create App needed directories
+createDirectory("logs");
+createDirectory("files");
+createDirectory("public");
+copyFile(path.join(__dirname, ".env-example"), path.join(__dirname, ".env"));
+
 new (require("./app/app"))();
