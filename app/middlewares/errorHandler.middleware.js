decabb41a8d35f21546fad3335adbb7ca1efc687
Mr-Bie <Mrb800424@gmail.com>
2022-11-28 11:37:37 +0330

    fix: error handler message fixed

--- app/middlewares/errorHandler.middleware.js
+++ app/middlewares/errorHandler.middleware.js
@@ -3,5 +3,9 @@ const logger = require("../../services/logger.service");
 
 module.exports = (err, req, res, next) => {
   logger.error(err);
-  return res.error({}, req.t("internal_server_error"), 500);
+  return res.error({
+    data: {},
+    message: req.t("namespace:internal_server_error"),
+    status: 500,
+  });
 };
